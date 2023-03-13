e0b9a721e8bc02d3db182a3345dea30a8470a958
monirsaikat <moniruzzamansaikat@yahoo.com>
2023-03-14 00:04:55 +0600

    first commit

---
+++ database/migrations/2023_03_13_173203_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->integer('customer_id')->nullable();
+            $table->double('total_cost')->default(0);
+            $table->text('details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
